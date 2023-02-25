d1e07b3942aac7d951ec782a5aa1be5084cb451b
greyseth <anargya2gilland@gmail.com>
2023-02-25 17:05:16 +0700

    first commit

---
+++ scripts/homepage.js
@@ -0,0 +1,73 @@
+const toPush = [
+  { name: "bio-main", pos: 300, change: "bio-in" },
+  { name: "likes", pos: 600, change: "likes-dislikes-in" },
+  { name: "dislikes", pos: 600, change: "likes-dislikes-in" },
+  { name: "top-bar", pos: 750, change: "decor-in" },
+  { name: "bottom-bar", pos: 750, change: "decor-in" },
+];
+toPush.forEach((p) => scrollAnims.push(p));
+
+let isZooming = false;
+let zoomedIn;
+
+function showText(e) {
+  if (isZooming) return;
+
+  isZooming = true;
+  zoomedIn = e;
+  const el = document.getElementById(e);
+
+  el.classList.remove("desc-in");
+  el.classList.remove("desc-out");
+  // setTimeout(() => {
+  //   el.classList.add("desc-in");
+  // }, 1);
+  window.requestAnimationFrame(() => {
+    el.classList.add("desc-in");
+  });
+}
+
+function hideText() {
+  if (isZooming) {
+    const desc = document.getElementById(zoomedIn);
+
+    desc.classList.remove("desc-in");
+    // setTimeout(() => {
+    //   desc.classList.add("desc-out");
+    // }, 1);
+    window.requestAnimationFrame(() => {
+      desc.classList.add("desc-out");
+    });
+
+    isZooming = false;
+  }
+}
+
+window.addEventListener("keydown", (e) => {
+  hideText();
+});
+
+window.addEventListener("mousedown", () => {
+  hideText();
+});
+
+const lists = ["likes", "dislikes"];
+lists.forEach((li) => {
+  const list = document.getElementById(li);
+  list.addEventListener("click", () => {
+    let i = 0;
+    for (const child of list.children) {
+      if (child.innerHTML === "I LIKE..." || child.innerHTML === "I HATE...")
+        continue;
+
+      setTimeout(() => {
+        child.classList.remove("list-move");
+        window.requestAnimationFrame(() => {
+          child.classList.add("list-move");
+        });
+      }, 100 * i);
+
+      i++;
+    }
+  });
+});
---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+let firstScroll = true;
+let showHeader = false;
+let update = false;
+const header = document.querySelector("header");
+
+document.addEventListener("scroll", (e) => {
+  if (this.scrollY > 0 || firstScroll) {
+    showHeader = true;
+  } else {
+    showHeader = false;
+  }
+
+  updateHeader();
+  firstScroll = false;
+});
+
+function updateHeader() {
+  if (update === showHeader) return;
+
+  if (showHeader) {
+    header.classList.remove("header-out");
+    header.classList.remove("header-in");
+    setTimeout(() => {
+      header.classList.add("header-out");
+    }, 1);
+  } else {
+    header.classList.remove("header-out");
+    header.classList.remove("header-in");
+    setTimeout(() => {
+      header.classList.add("header-in");
+    }, 1);
+  }
+
+  update = showHeader;
+}
+
+// setInterval(updateHeader, 1000);
+window.addEventListener("keydown", (e) => {
+  if (e.key === "t") {
+    alert(`scroll is at position ${this.scrollY}`);
+  }
+});
+
+const scrollAnims = [];
+setInterval(() => {
+  scrollAnims.forEach((s) => {
+    if (this.scrollY >= s.pos) {
+      const el = document.getElementById(s.name);
+      window.requestAnimationFrame(() => {
+        el.classList.add(s.change);
+      });
+    }
+  });
+}, 500);
