b2a5a6ea82ddea68041e15015d8bed54540d27ae
greyseth <anargya2gilland@gmail.com>
2023-02-28 12:04:17 +0700

    (Almost) finished jobs page.

---
+++ scripts/jobs.js
@@ -0,0 +1,11 @@
+const toPush = [
+  { name: "itch-link", pos: 450, change: "link-in" },
+  { name: "offer", pos: 640, change: "offer-in" },
+  { name: "blender", pos: 1550, change: "blender-in" },
+  { name: "blender-2", pos: 1550, change: "blender-in" },
+  { name: "blender-3", pos: 1550, change: "blender-in" },
+  { name: "blender-4", pos: 1550, change: "blender-in" },
+  { name: "blender-5", pos: 1550, change: "blender-in" },
+  { name: "blender-6", pos: 1550, change: "blender-in" },
+];
+toPush.forEach((s) => scrollAnims.push(s));
