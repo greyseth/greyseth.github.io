e42e6f02b8101a5d3afd6672184454876ff330d3
greyseth <anargya2gilland@gmail.com>
2023-03-01 10:13:44 +0700

    added interesting movies section.

--- scripts/interests.js
+++ scripts/interests.js
@@ -1,8 +1,42 @@
-const bgItems = ["d2", "re4", "oot", "p3", "sc4", "hl"];
-function enableBG(logo, bg) {
-  document.querySelector(
-    ".games"
-  ).style.backgroundImage = `url("../img/gaming/${bg}")`;
+const toPush = [
+  { name: "gamesDesc", pos: 700, change: "games-extra-in" },
+  { name: "gamesDesc2", pos: 710, change: "games-extra-in" },
+  { name: "moviesDesc", pos: 1800, change: "games-extra-in" },
+];
+toPush.forEach((s) => scrollAnims.push(s));
+
+const bgItems = [
+  "d2",
+  "re4",
+  "oot",
+  "p3",
+  "sc4",
+  "hl",
+  "t2",
+  "btff",
+  "bb",
+  "gb",
+  "bt",
+  "ram",
+  "star",
+];
+function enableBG(logo, bg, target = "games") {
+  let path = "";
+  switch (target) {
+    case "games":
+      path = "gaming";
+      break;
+    case "movies":
+      path = "movies";
+      break;
+    case "music":
+      path = "music";
+      break;
+  }
+
+  document.getElementById(
+    target
+  ).style.backgroundImage = `url("../img/${path}/${bg}")`;
 
   bgItems.forEach((b) => {
     if (b === logo) {
@@ -11,4 +45,8 @@ function enableBG(logo, bg) {
       document.getElementById(b).classList.remove("img-selected");
     }
   });
+}
+
+function reverse(target) {
+  document.getElementById(target).classList.toggle("reversed");
 }
