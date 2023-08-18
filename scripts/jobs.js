aaed4936fd76aa5f1604dbd46c5635dbbe5aed4d
greyseth <anargya2gilland@gmail.com>
2023-08-18 20:00:25 +0700

    Added latest projects section

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -7,5 +7,41 @@ const toPush = [
   { name: "blender-4", pos: 1550, change: "blender-in" },
   { name: "blender-5", pos: 1550, change: "blender-in" },
   { name: "blender-6", pos: 1550, change: "blender-in" },
+  { name: "latest-proj", pos: 2250, change: "latest-in" },
 ];
 toPush.forEach((s) => scrollAnims.push(s));
+
+const galleryFolder = "./img/latestproj";
+let selectedImg = `${galleryFolder}/CoverHor.jpg`;
+const galleryImages = [
+  `${galleryFolder}/level1.jpg`,
+  `${galleryFolder}/level3.jpg`,
+  `${galleryFolder}/level2.2.jpg`,
+  `${galleryFolder}/level3.3.jpg`,
+  `${galleryFolder}/level4.jpg`,
+];
+
+function updateGalleryImages() {
+  document.querySelector("#proj-gallery").innerHTML = "";
+
+  document.querySelector(".selected-image").src = selectedImg;
+  galleryImages.forEach((img) => {
+    const HTML = `<img src="${img}" onclick="viewGalleryImage('${img}')"/>`;
+    document
+      .querySelector("#proj-gallery")
+      .insertAdjacentHTML("beforeend", HTML);
+  });
+}
+
+function viewGalleryImage(select) {
+  const selectedTemp = selectedImg;
+  const galleryTemp = [...galleryImages];
+
+  const targetIndex = galleryImages.findIndex((f) => f === select);
+  galleryImages[targetIndex] = selectedTemp;
+  selectedImg = galleryTemp[targetIndex];
+
+  updateGalleryImages();
+}
+
+updateGalleryImages();
