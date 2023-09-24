3398053357656c6f6da1b9d2ca273320a7aae193
greyseth <anargya2gilland@gmail.com>
2023-09-24 22:23:16 +0700

    started making jobs page responsive

--- scripts/gameengine.js
+++ scripts/gameengine.js
@@ -33,53 +33,58 @@ let p1AttackCountdown = 0;
 let movingX = { moving: false, speed: 0.5 };
 let movingY = { moving: false, speed: 0.5 };
 
-let update = false;
+let paused = false;
 
+//Input handling
 document.addEventListener("keydown", (e) => {
-  if (e.key === "a") {
-    dirInput.left = true;
-    addInput("p1", "back");
-  } else if (e.key === "d") {
-    dirInput.right = true;
-    addInput("p1", "forward");
-  } else if (e.key === "s") {
-    dirInput.down = true;
-    addInput("p1", "down");
-  }
-
-  if (e.key === "j") {
-    enemInput.left = true;
-    addInput("p2", "forward");
-  } else if (e.key === "l") {
-    enemInput.right = true;
-    addInput("p2", "back");
-  } else if (e.key === "k") {
-    enemInput.down = true;
-    addInput("p2", "down");
-  }
-
-  if (e.key === "c") {
-    if (lastInputs.p1.toString() === "forward,down,forward") {
-      updateState(ryu, "shoryu");
-      p1IsAttacking = true;
-      p1AttackCountdown = 650;
-      movingX.moving = true;
-      movingX.speed = 0.5;
-
-      lastInputs.p1 = [];
+  if (!paused) {
+    if (e.key === "a") {
+      dirInput.left = true;
+      addInput("p1", "back");
+    } else if (e.key === "d") {
+      dirInput.right = true;
+      addInput("p1", "forward");
+    } else if (e.key === "s") {
+      dirInput.down = true;
+      addInput("p1", "down");
     }
 
-    if (
-      (lastInputs.p1[0] === "down" && lastInputs.p1[1] === "forward") ||
-      (lastInputs.p1[1] === "down" && lastInputs.p1[2] === "forward")
-    ) {
-      updateState(ryu, "hado");
-      p1IsAttacking = true;
-      p1AttackCountdown = 400;
+    if (e.key === "j") {
+      enemInput.left = true;
+      addInput("p2", "forward");
+    } else if (e.key === "l") {
+      enemInput.right = true;
+      addInput("p2", "back");
+    } else if (e.key === "k") {
+      enemInput.down = true;
+      addInput("p2", "down");
+    }
 
-      lastInputs.p1 = [];
+    if (e.key === "c") {
+      if (lastInputs.p1.toString() === "forward,down,forward") {
+        updateState(ryu, "shoryu");
+        p1IsAttacking = true;
+        p1AttackCountdown = 650;
+        movingX.moving = true;
+        movingX.speed = 0.5;
+
+        lastInputs.p1 = [];
+      }
+
+      if (
+        (lastInputs.p1[0] === "down" && lastInputs.p1[1] === "forward") ||
+        (lastInputs.p1[1] === "down" && lastInputs.p1[2] === "forward")
+      ) {
+        updateState(ryu, "hado");
+        p1IsAttacking = true;
+        p1AttackCountdown = 400;
+
+        lastInputs.p1 = [];
+      }
     }
   }
+
+  if (e.key === "p") paused = !paused;
 });
 
 document.addEventListener("keyup", (e) => {
@@ -180,7 +185,16 @@ function p2Update() {
 }
 
 setInterval(() => {
-  if (!update) return;
+  if (paused) {
+    document.querySelector(".sf-pause").hidden = false;
+    ryu.style.animationPlayState = "paused";
+    ken.style.animationPlayState = "paused";
+    return;
+  } else {
+    document.querySelector(".sf-pause").hidden = true;
+    ryu.style.animationPlayState = "running";
+    ken.style.animationPlayState = "running";
+  }
 
   p1Update();
   p2Update();
