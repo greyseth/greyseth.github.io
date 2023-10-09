1642a3206a0ecf01e9601e49fcbba408d6d0ca26
greyseth <anargya2gilland@gmail.com>
2023-10-09 10:39:02 +0700

    almost finished test your might

--- scripts/gameengine2.js
+++ scripts/gameengine2.js
@@ -1,11 +1,20 @@
 let mkPaused = false;
 
 let started = false;
+let hasHit = false;
 let meter = 0;
 
-const meterSpeed = 10;
-const meterAdd = 15;
+const meterSpeed = 11;
+const meterAdd = 20;
 const meterMax = 100;
+
+//in miliseconds
+const hitAnimTime = 700;
+const postHitTime = 2500;
+
+const char = document.querySelector(".scorps");
+const fillBar = document.getElementById("fill");
+const target = document.querySelector(".target");
 
 let pressing = false;
 
@@ -18,17 +27,43 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+function hit() {
+  meter = meterMax;
+  hasHit = true;
+
+  char.classList.remove("s-idle");
+  char.classList.add("s-hit");
+
+  setTimeout(() => {
+    if (meter >= meterMax) {
+      target.classList.add("target-broken");
+    }
+  }, hitAnimTime);
+
+  // setTimeout(() => {
+  //   char.classList.remove("s-hit");
+  //   char.classList.add("s-idle");
+  // }, postHitTime);
+}
+
 function mkUpdate() {
+  if (hasHit) return;
+
+  if (started && (meter >= meterMax || meter <= -20)) {
+    hit();
+    return;
+  }
+
   if (pressing) {
     meter += meterAdd;
     pressing = false;
   }
 
-  if (started) {
+  if (started && meter > 0) {
     meter -= meterSpeed;
   }
 
-  document.getElementById("tempdisplay").textContent = meter;
+  fillBar.style.height = `${meter}%`;
 }
 
 setInterval(mkUpdate, 100);
